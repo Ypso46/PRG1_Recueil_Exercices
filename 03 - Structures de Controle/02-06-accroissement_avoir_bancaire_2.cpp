b87e8963a2ebe5c28349045ee7f7393d47441311
Greg <gd.temp@pm.me>
2023-10-07 19:31:18 +0200

    Exercise 02.06 is done and it works

--- 03 - Structures de Controle/02-06-accroissement_avoir_bancaire_2.cpp
+++ 03 - Structures de Controle/02-06-accroissement_avoir_bancaire_2.cpp
@@ -1,3 +1,31 @@
-//
-// Created by Gregory Daguerre on 05.10.2023.
-//
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+
+    double montantInitial, tauxInteret, nombreAnnees;
+    do {
+        std::cout << "Saisissez un montant initial : " << std::endl;
+        std::cin >> montantInitial;
+    } while (montantInitial <= 1000);
+
+    do {
+        std::cout << "Saisissez un taux d'intérêt : " << std::endl;
+        std::cin >> tauxInteret;
+    } while (tauxInteret <= -5 || tauxInteret >= 50);
+
+    do {
+        std::cout << "Saisissez un nombre d'années : " << std::endl;
+        std::cin >> nombreAnnees;
+    } while (nombreAnnees <= 0);
+
+    double nouveauCapital = montantInitial;
+
+    for (int i = 0; i < nombreAnnees; ++i) {
+        nouveauCapital += (nouveauCapital * tauxInteret) / 100;
+    }
+
+    std::cout << "Le montant disponible après " << nombreAnnees << " est de " <<  nouveauCapital << " CHF" << std::endl;
+
+    return EXIT_SUCCESS;
+}
